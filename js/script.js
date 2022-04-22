1777c7e51f2254b1947e3afeeb8c67cf1633ad09
whitevans-eth <whitevans.eth@gmail.com>
2022-04-21 21:14:39 -0400

    add smooth scrolling, update some comments

---
+++ js/script.js
@@ -0,0 +1,5 @@
+if (document.getElementById('my-work-link')) {
+  document.getElementById('my-work-link').addEventListener('click', () => {
+    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+  })
+}
